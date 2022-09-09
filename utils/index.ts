4c8c772dabe4dfb5a340028109966b00e962a2de
Christian Birkenmaier <christian.birkenmaier@qbeyond.de>
2022-09-09 12:09:11 +0200

    Bug: checking for LS

--- utils/index.ts
+++ utils/index.ts
@@ -3,10 +3,12 @@ import { IssueState, RepositoryQuery } from "../generated/graphql";
 export const saveToLocalStorage = (
   data: NonNullable<RepositoryQuery["repository"]>[]
 ) => {
+  if (!localStorage) return;
   return localStorage.setItem("localRepositories", JSON.stringify(data));
 };
 export const loadFromLocalStorage = ():
   | NonNullable<RepositoryQuery["repository"]>[] => {
+  if (!localStorage) return [];
   const stringifiedRepositories = localStorage.getItem("localRepositories");
   return stringifiedRepositories ? JSON.parse(stringifiedRepositories) : [];
 };
